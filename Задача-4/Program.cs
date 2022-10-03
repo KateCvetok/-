a1eb785441e70ba4b74dee673fc8b0091b25dbbf
kate <cvetok_spb@bk.ru>
2022-10-03 22:31:49 +0300

    Задача-4

---
+++ Задача-4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 ==0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
