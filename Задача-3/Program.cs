0b1edfecd75e6e9bbca353f98356ff3e3a1585c3
kate <cvetok_spb@bk.ru>
2022-10-03 22:15:59 +0300

    Задача-3

---
+++ Задача-3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int c = int.Parse(Console.ReadLine());
+int max;
+
+if (a>b)
+{
+    max=a;
+}
+else
+{
+    max=b;
+}
+if(a>c)
+{
+    max=a;
+}
+else
+{
+    max=c;
+}
+if(b>c)
+{
+    max=b;
+}
+else
+{
+    max=c;
+}
+Console.WriteLine("Максимальное число " +max);
