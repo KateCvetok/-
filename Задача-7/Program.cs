d7e80e60ea10c1995393c8e22d86661d0bb21dd8
kate <cvetok_spb@bk.ru>
2022-10-04 09:44:23 +0300

    Задача-7

---
+++ Задача-7/Program.cs
@@ -0,0 +1,16 @@
+﻿int x;
+int y;
+int z;
+int a;
+Console.Write("Введите трёхзначное число: ");
+a = Convert.ToInt32(Console.ReadLine());
+while ((a > 999) | (a < 100))
+{
+   Console.Write("Ошибка!Введите число от 100 до 999: ");
+   a = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine();
+}
+z = a / 100;
+x = (a / 10)%10;
+y = a % 10;
+Console.WriteLine("Вторая цифра " +x);
