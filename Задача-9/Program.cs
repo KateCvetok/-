4475d2c675272dc31fb58680a161765e003716f9
kate <cvetok_spb@bk.ru>
2022-10-05 19:51:08 +0300

    Задача-9

--- Задача-9/Program.cs
+++ Задача-9/Program.cs
@@ -1,20 +1,9 @@
-﻿int x;
-int y;
-int z;
-int a;
-Console.Write("Введите трёхзначное число: ");
-a = Convert.ToInt32(Console.ReadLine());
-while ((a > 999) | (a < 100))
-{
-   Console.Write("Ошибка!Введите число от 100 до 999: ");
-   a = Convert.ToInt32(Console.ReadLine());
-   Console.WriteLine();
-}
-z = a / 100;
-x = (a / 10)%10;
-y = a % 10;
-Console.WriteLine("Вторая цифра " +x);
-string MyString = "zxy";
-Console.WriteLine(MyString.Remove(1,1));
+﻿Console.WriteLine("введите трехзначное число "); // от 100 до 999
+int a = int.Parse(Console.ReadLine());
 
-// Здесь не поняла как удалить вторую цифру. Подскажите пожалуйста
+int result = (a/10) % 10;
+ Console.WriteLine(result);
+
+  // int delete = result;
+// var str = a.ToString();
+// a = int.Parse(str.Remove(str.Length - delete, 1));
