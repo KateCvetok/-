f21949d2a7e8d86b1347e369ce77d2aa00ca9538
kate <cvetok_spb@bk.ru>
2022-10-05 19:19:31 +0300

    Задача-5

---
+++ Задача-5/Program.cs
@@ -0,0 +1,16 @@
+﻿ int N = Convert.ToInt32(Console.ReadLine());
+     int count = 0; // счетчик четных цифр
+     for (int i = 1; i <= N; i++) 
+     {
+     int n = i; 
+     while (n > 0) 
+     {
+     int ch = n % 2; 
+     if (ch % 2 == 0) 
+     {
+     count++; // увеличиваем счетчик на 1 при совпадении условия
+     }
+     n = n / 10; // избавляемся от последней цифры
+     }
+     }
+     Console.WriteLine(count);
