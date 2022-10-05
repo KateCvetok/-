80b0e68d3d76a13870d435cb92b632f9fac35ee0
kate <cvetok_spb@bk.ru>
2022-10-05 11:47:42 +0300

    Задача-10

---
+++ Задача-10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите первое число");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double b = double.Parse(Console.ReadLine());
+double c = a/b;
+
+if (a % b != 0)
+{
+    Console.WriteLine("Число b не является кратным числу a");
+   }
+else
+{
+    Console.WriteLine("Число b  является кратным числу a");
+    
+}
