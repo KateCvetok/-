c7924ae686c75690ca442e644dd585776256b7b1
kate <cvetok_spb@bk.ru>
2022-10-05 19:09:33 +0300

    Изменения в задаче-7

--- Задача-7/Program.cs
+++ Задача-7/Program.cs
@@ -1,17 +1,6 @@
 ﻿// Показать вторую цифру трёхзначного числа
-int x;
-int y;
-int z;
-int a;
-Console.Write("Введите трёхзначное число: ");
-a = Convert.ToInt32(Console.ReadLine());
-while ((a > 999) | (a < 100))
-{
-   Console.Write("Ошибка!Введите число от 100 до 999: ");
-   a = Convert.ToInt32(Console.ReadLine());
-   Console.WriteLine();
-}
-z = a / 100;
-x = (a / 10)%10;
-y = a % 10;
-Console.WriteLine("Вторая цифра " +x);
+Console.WriteLine("введите трехзначное число "); // от 100 до 999
+int a = int.Parse(Console.ReadLine());
+
+ Console.WriteLine((a/10) % 10);
+  
