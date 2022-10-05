2e22875a830c55fe210dd42be232c7c2c98aac75
kate <cvetok_spb@bk.ru>
2022-10-05 11:59:43 +0300

    внесли измененеия в задачу-2

--- Задача-2/Program.cs
+++ Задача-2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите номер дня недели"); // 1 2 3 4 5 6 7 
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (a == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (a == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (a == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (a == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (a == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (a == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
