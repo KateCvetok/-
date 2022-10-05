f110167d1ee46f9ec9365dedf5fc4475ef7cc620
kate <cvetok_spb@bk.ru>
2022-10-05 11:16:09 +0300

    Внесли изменения в задачу-3

--- Задача-3/Program.cs
+++ Задача-3/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("введите первое число");
+﻿// Найти максимальное из трех чисел
+Console.WriteLine("введите первое число");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("введите второе число");
 int b = int.Parse(Console.ReadLine());
